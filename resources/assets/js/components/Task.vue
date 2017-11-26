57a6fb4aca3bc41cfce51c9e2f565e9735134205
Onwuzor Victor <onwuzorvictor@outlook.com>
2017-11-26 14:58:40 +0100

    Vue child component of Todo component

---
+++ resources/assets/js/components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="item">
+		<input type="checkbox">
+        <p v-text="task.task"></p>
+        <span class="flex">{{ task.hour }}:{{ task.minute }} {{ task.period }}</span>
+        <button class="btn btn-sm btn-danger" @click="deleteTodo">
+        	<i class="fa fa-trash"></i>
+        </button>
+	</div>
+</template>
+<script>
+	export default {
+		props: ['task'],
+		methods: {
+			deleteTodo() {
+				axios.delete('/todo/' + this.task.id);
+				this.$emit('deleted', this.task.id);
+
+				flash('Task has been deleted', 'danger');
+			}
+		}
+	}
+</script>
