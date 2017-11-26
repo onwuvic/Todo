1c47b998a63ce12ff311bfb5fdb129f2847ff477
Onwuzor Victor <onwuzorvictor@outlook.com>
2017-11-26 14:57:53 +0100

    Vue component for todo list

---
+++ resources/assets/js/components/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+      <div class="col-md-12">
+        <div v-for="(task, index) in todo">
+            <task :task="task" @deleted="remove(index)"></task> 
+        </div>
+      </div>
+    </div>  
+</template>
+<script>
+	import Task from './Task.vue';
+    export default {
+    	components: { Task },
+    	props: ['todos'],
+    	data() {
+    		return {
+    			todo: this.todos
+    		}
+    	},
+    	methods: {
+    		remove(index) {
+    			this.todo.splice(index, 1);
+    		}
+    	}
+    }
+</script>
